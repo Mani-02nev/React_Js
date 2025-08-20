fac799c28b53fb3dd33feb1f0590bb59f9b69061
Mani <karuppasamy.murugesan02@gmail.com>
2025-08-20 21:55:52 +0530

    first commit on @Mani

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Exam App 🚀</h1>
+      <p>This is the React part of your project.</p>
+      <p>
+        You can also open:
+        <br />
+        <a href="/login.html">Login Page</a> <br />
+        <a href="/admin.html">Admin Page</a> <br />
+        <a href="/exam.html">Exam Page</a>
+      </p>
+    </div>
+  )
+}
+
+export default App
