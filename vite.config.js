3daac41eb1fd381a838742c0cb90f20b4dfa6ac2
MANI <karuppasamy.murugesan02@gmail.com>
2025-08-21 03:21:57 +0000

    mani

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/React_Js/',
-})
+  base: '/React_Js/', // <-- your repository name
+    plugins: [react()],
+    });
