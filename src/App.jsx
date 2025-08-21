bbb5e2c08f0d67d740f226d25fd4a40b12cacbc0
Mani <karuppasamy.murugesan02@gmail.com>
2025-08-21 07:27:12 +0530

    Add static index.html and React pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,26 @@
-import { Routes, Route, Link } from 'react-router-dom'
-import './App.css'
+import { Routes, Route, Link } from 'react-router-dom';
+import './App.css';
 
 function App() {
-  const Login = () => <h1>Login Page</h1>
-  const Home = () => <h1>Home Page</h1>
-  const Admin = () => <h1>Admin Page</h1>
-  const Exam = () => <h1>Exam Page</h1>
+  const Login = () => <h1>Login Page</h1>;
+  const Admin = () => <h1>Admin Page</h1>;
+  const Exam = () => <h1>Exam Page</h1>;
 
   return (
     <div>
       <nav>
         <Link to="/login">Login</Link> |
-        <Link to="/home">Home</Link> |
         <Link to="/admin">Admin</Link> |
         <Link to="/exam">Exam</Link>
       </nav>
 
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
         <Route path="/admin" element={<Admin />} />
         <Route path="/exam" element={<Exam />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
